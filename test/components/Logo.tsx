9830c9f8a6540631a740c3aec107b1c673e26b54
daniele <daniele.pompa@20tab.com>
2020-11-04 21:09:02 +0100

    mock UseSelector and UseDispatch

--- test/components/Logo.tsx
+++ test/components/Logo.tsx
@@ -1,12 +1,11 @@
 import React, { FC } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector } from '../utils/hooks/redux'
 import styled from 'styled-components'
 
-import { State } from '../models/State'
 import { Theme } from '../models/Theme'
 
 const Logo: FC = (props) => {
-  const theme = useSelector<State, Theme>(state => state.theme)
+  const theme = useSelector(state => state.theme)
   const isLight = theme === Theme.light
   const imageSrc = isLight ? '/images/logo.png' : '/images/logo_negative.png'
 
--- test/components/ThemeSwitch.tsx
+++ test/components/ThemeSwitch.tsx
@@ -1,14 +1,13 @@
 import React, { FC } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from '../utils/hooks/redux'
 import styled from 'styled-components'
 
-import { State } from '../models/State'
 import { Theme } from '../models/Theme'
 import { changeTheme } from '../store/actions/theme'
 
 const ThemeSwitch: FC = () => {
   const dispatch = useDispatch()
-  const theme = useSelector<State, Theme>(state => state.theme)
+  const theme = useSelector(state => state.theme)
   const isLight = theme === Theme.light
   const handlePressTheme = () => dispatch(changeTheme(isLight ? Theme.dark : Theme.light))
 
